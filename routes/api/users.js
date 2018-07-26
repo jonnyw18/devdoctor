ae82c24246ab0b9811fa580bcd12dda9da88563f
Jonathan <jonny_ward@hotmail.com>
2018-07-26 22:24:50 +0100

    validation checks done on login/registration data

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // Load User Model
 const User = require('../../models/User');
@@ -25,13 +26,13 @@ router.post('/register', (req, res) => {
 
   // Check Validation
   if (!isValid) {
-    erros.email = 'Email already exists';
     return res.status(400).json(errors);
   }
 
   User.findOne({email: req.body.email}).then((user) => {
     if (user) {
-      return res.status(400).json({email: 'Email already exists'})
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: '200', // Avatar size
@@ -55,6 +56,14 @@ router.post('/register', (req, res) => {
 // @desc    Login user / Returning the JWT token
 // @access  Public
 router.post('/login', (req, res) => {
+
+  const {isValid, errors} = validateLoginInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -62,7 +71,8 @@ router.post('/login', (req, res) => {
   User.findOne({email}).then(user => {
     // Check for user
     if (!user) {
-      return res.status(404).send({email: 'User email not found'})
+      errors.email = 'User not found'
+      return res.status(404).send(errors)
     }
 
     // Check Password
@@ -85,7 +95,8 @@ router.post('/login', (req, res) => {
           })
         });
       } else {
-        return res.status(400).json({password: 'Password incorrect'})
+        errors.password = 'Password incorrect'
+        return res.status(400).json(errors)
       }
     })
   })
